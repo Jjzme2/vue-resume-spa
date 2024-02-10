eaf90b19c73ae03c637f0a378260b1bbae2d1e17
Jjzme2 <jjzettler@gmail.com>
2024-02-10 10:31:48 -0600

    Add Socials

--- src/components/app/layoutElements/MainLayout.vue
+++ src/components/app/layoutElements/MainLayout.vue
@@ -3,28 +3,66 @@
     <header class="header" v-if="$slots.header">
       <quoteDisplay></quoteDisplay>
     </header>
+
     <main class="body">
       <slot></slot>
     </main>
+
     <footer class="footer" v-if="$slots.footer">
-      <footer>
+      <div class="footer-left">
+        <socials :links="socials"></socials>
+      </div>
+
+      <div class="footer-center">
         <p>&copy; {{ yearTag }}</p>
-        <!-- <span>yearTag</span> -->
-      </footer>
+      </div>
+
+      <div class="footer-right">
+        <ul>
+          <li>Powered by Vue.js</li>
+          <li>Hosted By Github Pages.</li>
+          <li>Domain provided by squarespace.</li>
+          <li>Application developed by Jj Zettler (ILYTAT)</li>
+        </ul>
+      </div>
     </footer>
   </div>
 </template>
 <script>
 import quoteDisplay from "@/components/app/mainElements/display/QuoteDisplay.vue";
+import socials from "@/components/app/mainElements/display/SocialLinks.vue";
 export default {
   name: "MainLayout",
   components: {
     quoteDisplay,
+    socials,
   },
   data() {
     return {
       currentYear: new Date().getFullYear(),
       startYear: 2024,
+      socials: [
+        {
+          link: "https://www.linkedin.com/in/jjilytatgames/",
+          network: "linkedin",
+        },
+        {
+          link: "https://www.github.com/jjzme2",
+          network: "github",
+        },
+        // {
+        //   link: "https://www.twitter.com/your-twitter",
+        //   network: "twitter",
+        // },
+        // {
+        //   link: "https://www.instagram.com/your-instagram",
+        //   network: "instagram",
+        // },
+        // {
+        //   link: "https://www.facebook.com/your-facebook",
+        //   network: "facebook",
+        // },
+      ],
     };
   },
   computed: {
---
+++ src/components/app/mainElements/display/SocialLinks.vue
@@ -0,0 +1,36 @@
+<template>
+  <ul class="social-links">
+    <li v-for="link in links" :key="link.network">
+      <a :href="link.link" :target="'blank'">
+        <img :src="getImageUrl(link.network)" :alt="link.network" />
+      </a>
+    </li>
+  </ul>
+</template>
+
+<script>
+export default {
+  props: {
+    links: {
+      type: Object,
+      required: true,
+      validator: (value) => {
+        return (
+          Object.prototype.hasOwnProperty.call(value, "Link") &&
+          Object.prototype.hasOwnProperty.call(value, "Network")
+        );
+      },
+    },
+  },
+  methods: {
+    getImageUrl(iconName) {
+      const context = require.context(
+        "@/assets/images/social",
+        false,
+        /\.png$/
+      );
+      return context(`./${iconName}.png`);
+    },
+  },
+};
+</script>
